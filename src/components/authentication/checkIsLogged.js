192c8da04cfc159c80b4a6bb90d28ce0b658ec17
Roberto Dias <robertodias@gmail.com>
2017-10-15 22:33:55 -0200

    Adjust basic React to ES6 and ESLint (components/authentication - content)

--- src/components/authentication/checkIsLogged.js
+++ src/components/authentication/checkIsLogged.js
@@ -1,46 +1,52 @@
-"use strict"
-
-//IMPORT REACT
+// IMPORT REACT
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {browserHistory} from 'react-router';
 
-//OUR ACTIONS
+// Update REACT PropTypes
+import PropTypes from 'prop-types';
+
+// OUR ACTIONS
 import {setRedirectUrl} from '../../actions/loginActions';
 
 class CheckIsLogged extends React.Component {
-
   componentDidMount() {
-    const { dispatch, currentURL } = this.props
+    const { currentUrl } = this.props;
     if (!this.props.user) {
       // set the current url/path for future redirection (we use a Redux action)
       // then redirect (we use a React Router method)
-      this.props.setRedirectUrl(currentURL);
-      browserHistory.replace("/login");
+      this.props.setRedirectUrl(currentUrl);
+      browserHistory.replace('/login');
     }
   }
 
   render() {
     if (this.props.user) {
-      return this.props.children
-    } else {
-      return null
+      return this.props.children;
     }
+    return null;
   }
 }
 
 function mapStateToProps(state, ownProps) {
   return {
     user: state.login.user,
-    currentURL: ownProps.location.pathname
-  }
+    currentUrl: ownProps.location.pathname,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
-    setRedirectUrl
+    setRedirectUrl,
   }, dispatch);
 }
+
+CheckIsLogged.propTypes = {
+  currentUrl: PropTypes.any,
+  setRedirectUrl: PropTypes.any,
+  user: PropTypes.any,
+  children: PropTypes.node.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CheckIsLogged);
--- src/components/authentication/logout.js
+++ src/components/authentication/logout.js
@@ -1,74 +1,82 @@
-"use strict"
-
-//IMPORT REACT
+// IMPORT REACT
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {browserHistory} from 'react-router';
 import {Modal, Button} from 'react-bootstrap';
-import {findDOMNode} from 'react-dom';
 
-//OUR ACTIONS
+// Update REACT PropTypes
+import PropTypes from 'prop-types';
+
+// OUR ACTIONS
 import {logout, closeLogoutModal, openLogoutModal} from '../../actions/loginActions';
 
 class Logout extends React.Component {
-
   componentDidMount() {
     this.props.openLogoutModal();
     if (!this.props.user) {
-      browserHistory.replace("/login");
+      browserHistory.replace('/login');
     }
   }
 
   componentDidUpdate() {
     if (!this.props.user) {
-      browserHistory.replace("/login");
+      browserHistory.replace('/login');
     }
   }
 
+  render() {
+    return (
+      <Modal show={this.props.showLogoutModal}>
+        <Modal.Header>
+          <Modal.Title>Logout Warning</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          Are you sure you want to leave the DC$ application?
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={(this.cancel.bind(this))}>Cancel</Button>
+          <Button onClick={(this.confirm.bind(this))}bsStyle="primary">Confirm</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
   confirm() {
-    browserHistory.replace("/");
+    browserHistory.replace('/');
     this.props.logout();
   }
 
   cancel() {
     this.props.closeLogoutModal();
   }
-
-  render() {
-    return (
-        <Modal show={this.props.showLogoutModal}>
-          <Modal.Header>
-            <Modal.Title>Logout Warning</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            Are you sure you want to leave the DC$ application?
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button onClick={(this.cancel.bind(this))}>Cancel</Button>
-            <Button onClick={(this.confirm.bind(this))}bsStyle="primary">Confirm</Button>
-          </Modal.Footer>
-        </Modal>
-    )
-  }
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
   return {
     user: state.login.user,
-    showLogoutModal: state.login.showLogoutModal
-  }
+    showLogoutModal: state.login.showLogoutModal,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
     logout,
     closeLogoutModal,
-    openLogoutModal
+    openLogoutModal,
   }, dispatch);
 }
 
+Logout.propTypes = {
+  openLogoutModal: PropTypes.any,
+  closeLogoutModal: PropTypes.any,
+  showLogoutModal: PropTypes.any,
+  setRedirectUrl: PropTypes.any,
+  logout: PropTypes.any,
+  user: PropTypes.any,
+  children: PropTypes.node.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Logout);
